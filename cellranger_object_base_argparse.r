85aac32824907d3cd177c348b0fed3f71185a791
furkanhisto <Furkan.Oezkezer@histogenex.com>
2021-05-26 09:41:45 +0200

    creating seurat object with arg parse method

---
+++ cellranger_object_base_argparse.r
@@ -0,0 +1,18 @@
+#!/usr/bin/env Rscript 
+
+library(Seurat)
+
+# to gather the input files from the channel. 
+#with make option you can write a shebang script on the file. we use this to gather the files emitted from the channel and run the script on it
+args = commandArgs(trailingOnly=TRUE)
+
+print(args)
+
+sample<- args[1]
+
+print(paste0("Sample to process: ", sample))
+
+data <- Read10X(data.dir = ".")
+data <-  CreateSeuratObject(counts=data)
+
+saveRDS(data, paste0(sample, "_cellranger_seurat.rds"))
