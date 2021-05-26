c23e3f67c9c9a2e5a98ff8a562cb1be3036a2ad3
furkanhisto <Furkan.Oezkezer@histogenex.com>
2021-05-26 21:18:31 +0200

    unsuccessful to create seurat object

--- cellranger_object.r
+++ cellranger_object.r
@@ -6,13 +6,14 @@ library(optparse)
 # to gather the input files from the channel. 
 #with make option you can write a shebang script on the file. we use this to gather the files emitted from the channel and run the script on it
 option_list=list(
-  make_option("--sample",default="sample1",action="store",type='character',help="sample"))
+  make_option("--sample",default="sample1",action="store",type='character',help="sample")
+  make_option("--name",default="GEM",action="store",type='character',help="sample"))
 
 opt=parse_args(OptionParser(option_list=option_list))
 
 print(paste0("Sample to process: ", opt$sample))
 
-data <- Read10X(data.dir = ".")
+data <- Read10X(data.dir = opt$sample)
 data <-  CreateSeuratObject(counts=data)
 
-saveRDS(data, paste0(opt$sample, "_cellranger_seurat.rds"))
+saveRDS(data, paste0(opt$name, "_cellranger_seurat.rds"))
