df06f41ea50dc1f1574e0a17de55095c0098b642
furkanhisto <Furkan.Oezkezer@histogenex.com>
2021-05-28 23:42:24 +0200

    script to create seurat obj for bus_count

---
+++ seurat_kallisto_count.r
@@ -0,0 +1,19 @@
+#!/usr/bin/env Rscript 
+
+#packages
+library(BUSpaRse)
+library(Seurat)
+
+# to gather the input files from the channel. 
+#with make option you can write a shebang script on the file. we use this to gather the files emitted from the channel and run the script on it
+args = commandArgs(trailingOnly=TRUE)
+
+sample<- args[1]
+
+#reading file and creating seurat object
+res_mat <- read_count_output(".", name = "tcc", tcc = TRUE)
+
+seu <- CreateSeuratObject(res_mat, min.cells = 3, min.feature=200)
+
+saveRDS(res_mat, paste0(sample, "_seurat.rds"))
+
